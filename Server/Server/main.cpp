b45615acf2f364e18b15850aece89c0128c40300
TaiPham <115418952+taiphamhuu56ld3@users.noreply.github.com>
2023-12-25 16:39:34 +0700

    [FEAT] cpp: First init and plan to make connections

---
+++ Server/Server/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+int main()
+{
+	// Initialze winsock
+
+	// Create a socket
+
+	// Bind the ip address and port to a socket
+
+	// Tell Winsock the socket is for listening
+
+    // Wait for a connection
+
+    // Close listening socket
+
+    // While loop: accept and echo message back to client
+
+    // close the sock
+
+    // Shutdown winsock
+}
