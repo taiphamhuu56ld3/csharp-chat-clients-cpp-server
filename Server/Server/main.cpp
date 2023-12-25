245addc1f4ca105da0b7cf9967eb4cd15bb3bc30
TaiPham <115418952+taiphamhuu56ld3@users.noreply.github.com>
2023-12-25 16:46:36 +0700

    [FEAT] cpp: Bind the ip address and port

--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -27,6 +27,12 @@ int main()
 	}
 
 	// Bind the ip address and port to a socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton ....
+
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
 
 	// Tell Winsock the socket is for listening
 
