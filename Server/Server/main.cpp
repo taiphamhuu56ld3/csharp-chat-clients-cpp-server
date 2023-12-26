99806fb10f7ff801f36282741f3dc45161800ea4
TaiPham <115418952+taiphamhuu56ld3@users.noreply.github.com>
2023-12-26 22:38:01 +0700

    [CHORE] cpp: Change between tab and space

--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -7,34 +7,34 @@ using namespace std;
 
 int main()
 {
-	// Initialze winsock
+    // Initialze winsock
     WSADATA wsData;
-	WORD ver = MAKEWORD(2, 2);
+    WORD ver = MAKEWORD(2, 2);
 
-	int wsOk = WSAStartup(ver, &wsData);
-	if (wsOk != 0)
-	{
-		cerr << "Can't Initialize winsock! Quitting" << endl;
-		return 99;
-	}
+    int wsOk = WSAStartup(ver, &wsData);
+    if (wsOk != 0)
+    {
+        cerr << "Can't Initialize winsock! Quitting" << endl;
+        return 99;
+    }
 
-	// Create a socket
+    // Create a socket
     SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
-	if (listening == INVALID_SOCKET)
-	{
-		cerr << "Can't create a socket! Quitting" << endl;
-		return 99;
-	}
+    if (listening == INVALID_SOCKET)
+    {
+        cerr << "Can't create a socket! Quitting" << endl;
+        return 99;
+    }
 
-	// Bind the ip address and port to a socket
-	sockaddr_in hint;
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(54000);
-	hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton ....
+    // Bind the ip address and port to a socket
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(54000);
+    hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton ....
 
-	bind(listening, (sockaddr*)&hint, sizeof(hint));
+    bind(listening, (sockaddr*)&hint, sizeof(hint));
 
-	// Tell Winsock the socket is for listening
+    // Tell Winsock the socket is for listening
     listen(listening, SOMAXCONN);
 
     // Wait for a connection
